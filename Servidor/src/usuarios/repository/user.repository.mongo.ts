410264d8306f8b67860857d97793b4e708c4d7ae
diegoMV17 <diegomoreno3245@gmail.com>
2025-06-14 23:00:08 -0500

    Registro de Usuarios Y logeo de los mismos

--- Servidor/src/usuarios/repository/user.repository.mongo.ts
+++ Servidor/src/usuarios/repository/user.repository.mongo.ts
@@ -1,12 +1,27 @@
 import { UserRepository } from "../interfaces/user.repository";
 import { UserModel } from "../models/user.model";
-import { CreateUsertDto, UpdateUserDto } from "../interfaces/user.dto";
+import { CreateUsertDto, loginUserDto, UpdateUserDto } from "../interfaces/user.dto";
+import bcrypt from "bcrypt"
 
 export class UserRepositoryMongo implements UserRepository {
-  async createUser(data: CreateUsertDto): Promise<CreateUsertDto> {
-    const user = new UserModel(data);
-    await user.save();
-    return user.toObject();
+  
+  async createUser(data: CreateUsertDto){
+    const hashPassword = await bcrypt.hash(data.contraseña.toString(), 10);
+    const user = {...data, contraseña: hashPassword};
+    const createUser = await UserModel.create(user);
+    return createUser;
+  }
+
+  async loginUser(data: loginUserDto){
+    const user = await UserModel.findOne({ correo: data.correo });
+    if (!user) {
+      throw new Error("Invalid credentials")
+    }
+    const isPasswordValid = await bcrypt.compare(data.contraseña.toString(), user.contraseña.toString());
+    if (!isPasswordValid) {
+      throw new Error("Invalid credentials");
+    }
+    return user;
   }
 
   async getAllUsers(): Promise<CreateUsertDto[]> {
--- Servidor/src/usuarios/routes/user.routes.ts
+++ Servidor/src/usuarios/routes/user.routes.ts
@@ -25,6 +25,16 @@ userRoutes.post('/', async (req: Request, res: Response) => {
     res.status(500).json({ message: 'Error creating user', error });
   }
 });
+
+userRoutes.post('/login', async (req: Request, res: Response) => {
+  try {
+    const user = await userService.loginUser(req.body);
+    res.status(200).json(user);
+  } catch (error) {
+    res.status(401).json({ message: 'Invalid credentials', error });
+  }
+});
+
 userRoutes.put('/:id', async (req: Request, res: Response): Promise<void> => {
   try {
     const updatedUser = await userService.updateUser(req.params.id, req.body);
